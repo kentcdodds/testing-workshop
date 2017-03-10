406f433c76efda0d8cd512a8713f3e14f8d69624
Kent C. Dodds <kent@doddsfamily.us>
2017-03-10 11:29:30 -0700

    revamped e2e testing scripts. Simpler a little

--- cypress/utils.js
+++ cypress/utils.js
@@ -17,11 +17,11 @@ function getRandomUserData() {
 }
 
 function visitApp(route = '/') {
-  const hash = Cypress.env('E2E_DEV') ? '#' : ''
+  const hash = Cypress.env('E2E_DEV') ? '/#' : ''
   const clientUrl = Cypress.env('CLIENT_URL')
   const apiUrl = Cypress.env('API_URL')
   const query = `api-url=${encodeURIComponent(apiUrl)}`
-  const fullUrl = `${clientUrl}/${hash}${route}?${query}`
+  const fullUrl = `${clientUrl}${hash}${route}?${query}`
   return cy.visit(fullUrl)
 }
 
