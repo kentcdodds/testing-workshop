0ff9880346e6349c4ad4d0cf27b14246f7f36f5c
Kent C. Dodds <kent@doddsfamily.us>
2017-04-13 22:50:01 -0600

    generated

--- client/src/screens/__tests__/editor.js
+++ client/src/screens/__tests__/editor.js
@@ -1,31 +1,18 @@
-import React from 'react'
-import {mount} from 'enzyme'
 import {Component as Editor} from '../editor'
 
 // default case
 test('renders editor form by default', () => {
-  const wrapper = render()
-  expect(wrapper).toMatchSnapshot()
+  // TODO
 })
 
 // testing props
 test('renders the given title', () => {
-  const title = 'The day I dualed Lord Voldemort'
-  const wrapper = render({title})
-  expect(wrapper.find(sel('title')).node.value).toBe(title)
+  // TODO
 })
 
 // testing user input
 test('adds tag when the user hits enter', () => {
-  const newTag = 'interwebs'
-  const tagList = ['internet', 'web', 'network']
-  const wrapper = render({tagList})
-  const tagInput = wrapper.find(sel('tags'))
-  changeInputValue(tagInput, newTag)
-  keyUpInput(tagInput, 13)
-  const tagPills = wrapper.find(sel('tag-pills'))
-  expect(tagPills.children()).toHaveLength(4)
-  expect(tagPills.find(`[data-tag="${newTag}"]`)).toHaveLength(1)
+  // TODO
 })
 
 // This component doesn't make any subscriptions to test
@@ -34,34 +21,6 @@ test('adds tag when the user hits enter', () => {
 // often need to) but those are the only two other things
 // you need to test with React!
 
-function render(props = {}) {
-  const propsToUse = {
-    onLoad() {},
-    onSubmit() {},
-    onUnload() {},
-    articleSlug: '',
-    title: '',
-    description: '',
-    body: '',
-    tagList: [],
-    inProgress: false,
-    params: {},
-    ...props,
-  }
-  return mount(<Editor {...propsToUse} />)
-}
-
-function changeInputValue(input, value) {
-  input.simulate('change', {target: {value}})
-}
-
-function keyUpInput(input, keyCode) {
-  input.simulate('keyup', {keyCode})
-}
-
-function sel(id) {
-  return `[data-test="${id}"]`
-}
 
 //////// Elaboration & Feedback /////////
 /*
--- client/src/screens/__tests__/login.js
+++ client/src/screens/__tests__/login.js
@@ -1,55 +1,17 @@
-import React from 'react'
-import {mount} from 'enzyme'
 import {Component as Login} from '../login'
 
 test('renders login form by default', () => {
-  const wrapper = render()
-  expect(isSubmitButtonDisabled(wrapper)).toBe(false)
+  // TODO
 })
 
 test('disabled button when inProgress', () => {
-  const wrapper = render({inProgress: true})
-  expect(isSubmitButtonDisabled(wrapper)).toBe(true)
+  // TODO
 })
 
 test('shows list of errors when there are errors', () => {
-  const wrapper = render({
-    errors: {
-      'some-error': 'there was some error',
-      'some-other-error': 'there was some other error',
-    },
-  })
-  expect(getErrors(wrapper)).toEqual([
-    'some-error there was some error',
-    'some-other-error there was some other error',
-  ])
+  // TODO
 })
 
-function render(props = {}) {
-  const propsToUse = {
-    onSubmit() {},
-    onUnload() {},
-    inProgress: false,
-    errors: {},
-    ...props,
-  }
-  return mount(<Login {...propsToUse} />)
-}
-
-function isSubmitButtonDisabled(wrapper) {
-  const button = getSubmitButton(wrapper).getNode()
-  return button.disabled
-}
-
-function getSubmitButton(wrapper) {
-  return wrapper.find('button[type="submit"]')
-}
-
-function getErrors(wrapper) {
-  return Array.from(wrapper.find('.error-messages li').getNodes()).map(
-    n => n.textContent,
-  )
-}
 
 //////// Elaboration & Feedback /////////
 /*
