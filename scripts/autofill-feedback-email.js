93cc44a2ed939f7be91fb96c8829aad2377b010f
Kent C. Dodds <kent@doddsfamily.us>
2017-04-13 22:52:40 -0600

    add autofill-feedback-email

---
+++ scripts/autofill-feedback-email.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const replace = require('replace-in-file')
+
+const email = process.argv[2]
+
+if (!email) {
+  throw new Error('You must provide an email address as an argument')
+}
+
+const clientGlob = [
+  path.join(__dirname, '..', 'client/src/**/*.js'),
+  path.join(__dirname, '..', 'client/tests/**/*.js'),
+]
+
+const apiGlob = [
+  path.join(__dirname, '..', 'api/src/**/*.js'),
+  path.join(__dirname, '..', 'api/tests/**/*.js'),
+]
+
+const options = {
+  files: clientGlob.concat(apiGlob),
+  from: /&em=/,
+  to: `&em=${email}`,
+}
+
+replace(options).then(
+  changedFiles => {
+    console.log(`Updated ${changedFiles.length} with the email ${email}`)
+  },
+  error => {
+    console.error('Failed to update files')
+    console.error(error.stack)
+  }
+)
+
+// this is not transpiled
+/*
+  eslint
+  comma-dangle: [
+    2,
+    {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      functions: 'never'
+    }
+  ]
+ */
