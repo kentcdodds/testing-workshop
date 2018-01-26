bbc0c0c85bb8ce575d9302554bb084ebdf595753
Kent C. Dodds <kent@doddsfamily.us>
2018-01-25 22:08:55 -0700

    make controllers and unit test auth

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,72 +1,23 @@
 import {authMiddleware} from '../auth'
-import db from '../db'
+import * as postController from '../controllers/posts'
 
 function setupPostRoutes(router) {
-  const authorize = async (req, res, next) => {
-    const {authorId} = await db.getPost(req.params.id)
-    if (req.user.id === authorId) {
-      return next()
-    } else {
-      return res.status(403).send()
-    }
-  }
+  router.get('/', postController.getPosts)
+  router.get('/:id', postController.getPost)
+  router.post('/', authMiddleware.required, postController.createPost)
 
-  router.get('/', async (req, res) => {
-    const posts = await db.getPosts()
-    if (posts) {
-      res.json({posts})
-    } else {
-      res.status(404).send()
-    }
-  })
-
-  router.get('/:id', async (req, res) => {
-    const post = await db.getPost(req.params.id)
-    if (post) {
-      res.json({post})
-    } else {
-      res.status(404).send()
-    }
-  })
-
-  router.post('/', authMiddleware.required, async (req, res) => {
-    const post = await db.insertPost(req.body)
-    if (post) {
-      res.json({post})
-    } else {
-      res.status(404).send()
-    }
-  })
-
-  router.put('/:id', authMiddleware.required, authorize, async (req, res) => {
-    const post = await db.getPost(req.params.id)
-    if (!post) {
-      return res.status(404).send()
-    }
-    if (req.user.id !== post.authorId) {
-      return res.status(403).send()
-    }
-    const updatedPost = await db.updatePost(req.params.id, req.body)
-    if (updatedPost) {
-      return res.json({post: updatedPost})
-    } else {
-      // TODO: come up with a better status code...
-      return res.status(500).send()
-    }
-  })
+  router.put(
+    '/:id',
+    authMiddleware.required,
+    postController.authorize,
+    postController.updatePost,
+  )
 
   router.delete(
     '/:id',
     authMiddleware.required,
-    authorize,
-    async (req, res) => {
-      const post = await db.deletePost(req.params.id)
-      if (post) {
-        return res.json({post})
-      } else {
-        return res.status(404).send()
-      }
-    },
+    postController.authorize,
+    postController.deletePost,
   )
 }
 
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,65 +1,23 @@
-import {authMiddleware, userToJSON, getUserToken} from '../auth'
-import db from '../db'
+import {authMiddleware} from '../auth'
+import * as userController from '../controllers/users'
 
 function setupUserRoutes(router) {
-  const authorize = (req, res, next) => {
-    if (req.user.id === req.params.id) {
-      return next()
-    } else {
-      return res.status(403).send()
-    }
-  }
+  router.get('/', userController.getUsers)
 
-  router.get('/', async (req, res) => {
-    const users = await db.getUsers()
-    if (users) {
-      res.json({users: users.map(u => userToJSON(u))})
-    } else {
-      res.status(404).send()
-    }
-  })
+  router.get('/:id', authMiddleware.optional, userController.getUser)
 
-  router.get('/:id', authMiddleware.optional, async (req, res) => {
-    const user = await db.getUser(req.params.id)
-    if (user) {
-      res.json({
-        user: {
-          ...userToJSON(user),
-          token:
-            req.user && req.user.id === req.params.id
-              ? getUserToken(user)
-              : undefined,
-        },
-      })
-    } else {
-      res.status(404).send()
-    }
-  })
-
-  router.put('/:id', authMiddleware.required, authorize, async (req, res) => {
-    if (req.user.id !== req.params.id) {
-      return res.status(403).send()
-    }
-    const user = await db.updateUser(req.params.id, req.body)
-    if (user) {
-      return res.json({user: userToJSON(user)})
-    } else {
-      return res.status(404).send()
-    }
-  })
+  router.put(
+    '/:id',
+    authMiddleware.required,
+    userController.authorize,
+    userController.updateUser,
+  )
 
   router.delete(
     '/:id',
     authMiddleware.required,
-    authorize,
-    async (req, res) => {
-      const user = await db.deleteUser(req.params.id)
-      if (user) {
-        return res.json({user: userToJSON(user)})
-      } else {
-        return res.status(404).send()
-      }
-    },
+    userController.authorize,
+    userController.deleteUser,
   )
 }
 
