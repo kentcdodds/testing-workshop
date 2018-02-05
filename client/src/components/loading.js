d3329af0795cc505d22bdcf9b46f772ab767dffb
Kent C. Dodds <kent@doddsfamily.us>
2018-02-04 19:44:45 -0700

    fix linting issue

--- client/src/components/loading.js
+++ client/src/components/loading.js
@@ -2,21 +2,21 @@ import React from 'react'
 import glamorous from 'glamorous'
 import * as glamor from 'glamor'
 
-const loadingAnimation = props => {
-  const bounce = glamor.css.keyframes({
-    '0%': {
-      color: 'var(--black)',
-      boxShadow: 'var(--shadowHover)',
-    },
-    '100%': {
-      color: 'var(--green)',
-      boxShadow: 'var(--shadow)',
-    },
-  })
-  return {
-    animation: `${bounce} 0.7s infinite ease-in-out alternate`,
-  }
+const bounce = glamor.css.keyframes({
+  '0%': {
+    color: 'var(--black)',
+    boxShadow: 'var(--shadowHover)',
+  },
+  '100%': {
+    color: 'var(--green)',
+    boxShadow: 'var(--shadow)',
+  },
+})
+
+const loadingAnimation = {
+  animation: `${bounce} 0.7s infinite ease-in-out alternate`,
 }
+
 const LoadingContainer = glamorous.span(
   {
     position: 'fixed',
