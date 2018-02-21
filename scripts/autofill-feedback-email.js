b999bcf9d1b91e508711c4ee43a5eb44b332b092
Kent C. Dodds <kent@doddsfamily.us>
2018-02-21 08:20:54 -0600

    final cleanup

--- scripts/autofill-feedback-email.js
+++ scripts/autofill-feedback-email.js
@@ -1,35 +1,38 @@
 /* eslint no-console:0 */
 const path = require('path')
+const inquirer = require('inquirer')
 const replace = require('replace-in-file')
 
-const email = process.argv[2]
+inquirer
+  .prompt([
+    {
+      name: 'email',
+      message: `what's your email address?`,
+      validate(val) {
+        return Boolean(val) || 'email is required'
+      },
+    },
+  ])
+  .then(({email}) => {
+    const options = {
+      files: [
+        path.join(__dirname, '..', 'client/src/**/*.js'),
+        path.join(__dirname, '..', 'client/tests/**/*.js'),
+        path.join(__dirname, '..', 'server/src/**/*.js'),
+        path.join(__dirname, '..', 'server/tests/**/*.js'),
+        path.join(__dirname, '..', 'cypress/e2e/**/*.js'),
+      ],
+      from: /&em=\n/,
+      to: `&em=${email}`,
+    }
 
-if (!email) {
-  throw new Error('You must provide an email address as an argument')
-}
-
-const clientGlob = [
-  path.join(__dirname, '..', 'client/src/**/*.js'),
-  path.join(__dirname, '..', 'client/tests/**/*.js'),
-]
-
-const apiGlob = [
-  path.join(__dirname, '..', 'api/src/**/*.js'),
-  path.join(__dirname, '..', 'api/tests/**/*.js'),
-]
-
-const options = {
-  files: clientGlob.concat(apiGlob),
-  from: /&em=\n/,
-  to: `&em=${email}`,
-}
-
-replace(options).then(
-  changedFiles => {
-    console.log(`Updated ${changedFiles.length} with the email ${email}`)
-  },
-  error => {
-    console.error('Failed to update files')
-    console.error(error.stack)
-  },
-)
+    replace(options).then(
+      changedFiles => {
+        console.log(`Updated ${changedFiles.length} with the email ${email}`)
+      },
+      error => {
+        console.error('Failed to update files')
+        console.error(error.stack)
+      },
+    )
+  })
