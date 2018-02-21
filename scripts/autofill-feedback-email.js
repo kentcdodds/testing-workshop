c1322eb41781fbb503db830cf9f514131c2046d8
Kent C. Dodds <kent@doddsfamily.us>
2018-02-21 08:27:38 -0600

    improve setup to include email autofill stuff

--- scripts/autofill-feedback-email.js
+++ scripts/autofill-feedback-email.js
@@ -2,37 +2,47 @@
 const path = require('path')
 const inquirer = require('inquirer')
 const replace = require('replace-in-file')
+const isCI = require('is-ci')
 
-inquirer
-  .prompt([
-    {
-      name: 'email',
-      message: `what's your email address?`,
-      validate(val) {
-        return Boolean(val) || 'email is required'
+if (isCI) {
+  console.log(`Not running autofill feedback as we're on CI`)
+} else {
+  inquirer
+    .prompt([
+      {
+        name: 'email',
+        message: `what's your email address?`,
+        validate(val) {
+          if (!val) {
+            return 'email is required'
+          } else if (!val.includes('@')) {
+            return 'email requires an @ sign...'
+          }
+          return true
+        },
       },
-    },
-  ])
-  .then(({email}) => {
-    const options = {
-      files: [
-        path.join(__dirname, '..', 'client/src/**/*.js'),
-        path.join(__dirname, '..', 'client/tests/**/*.js'),
-        path.join(__dirname, '..', 'server/src/**/*.js'),
-        path.join(__dirname, '..', 'server/tests/**/*.js'),
-        path.join(__dirname, '..', 'cypress/e2e/**/*.js'),
-      ],
-      from: /&em=\n/,
-      to: `&em=${email}`,
-    }
+    ])
+    .then(({email}) => {
+      const options = {
+        files: [
+          path.join(__dirname, '..', 'client/src/**/*.js'),
+          path.join(__dirname, '..', 'client/test/**/*.js'),
+          path.join(__dirname, '..', 'server/src/**/*.js'),
+          path.join(__dirname, '..', 'server/test/**/*.js'),
+          path.join(__dirname, '..', 'cypress/e2e/**/*.js'),
+        ],
+        from: /&em=\n/,
+        to: `&em=${email}\n`,
+      }
 
-    replace(options).then(
-      changedFiles => {
-        console.log(`Updated ${changedFiles.length} with the email ${email}`)
-      },
-      error => {
-        console.error('Failed to update files')
-        console.error(error.stack)
-      },
-    )
-  })
+      replace(options).then(
+        changedFiles => {
+          console.log(`Updated ${changedFiles.length} with the email ${email}`)
+        },
+        error => {
+          console.error('Failed to update files')
+          console.error(error.stack)
+        },
+      )
+    })
+}
