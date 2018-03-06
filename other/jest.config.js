9fbbe8fd66315df46f0f7404556ea875d7a56d71
Kent C. Dodds <kent@doddsfamily.us>
2018-03-05 19:12:01 -0700

    include thumb-war in validation script

---
+++ other/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  coverageDirectory: './coverage',
+  collectCoverageFrom: [
+    '**/src/**/*.js',
+    '!**/__tests__/**',
+    '!**/node_modules/**',
+  ],
+  projects: ['./client', './server', './other/whats-a-mock'],
+}
--- other/whats-a-mock/jest.config.js
+++ other/whats-a-mock/jest.config.js
@@ -1,1 +1,4 @@
-module.exports = {}
+module.exports = {
+  displayName: 'mocks',
+  testEnvironment: 'node',
+}
