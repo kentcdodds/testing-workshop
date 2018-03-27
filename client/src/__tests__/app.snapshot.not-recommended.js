54733d15b1605da0b3dfc66b11e175dc3864d2a7
Kent C. Dodds <kent@doddsfamily.us>
2018-03-27 13:02:01 -0700

    add bad snapshot

---
+++ client/src/__tests__/app.snapshot.not-recommended.js
@@ -0,0 +1,49 @@
+/*
+ * This is an example of integration tests for the client
+ * They're still fairly react-specific, but less-so than
+ * the unit tests. They are also quite longer than
+ * unit tests. They cover more code than the unit tests
+ * per test.
+ */
+
+import React from 'react'
+import axiosMock from 'axios'
+import {renderWithRouter, flushPromises, generate} from 'client-test-utils'
+import {init as initAPI} from '../utils/api'
+import App from '../app'
+
+beforeEach(() => {
+  window.localStorage.removeItem('token')
+  axiosMock.__mock.reset()
+  initAPI()
+})
+
+test('snapshot', async () => {
+  // setup things to simulate being logged in
+  const {get} = axiosMock.__mock.instance
+  const fakeToken = 'my.fake.token'
+  window.localStorage.setItem('token', fakeToken)
+  initAPI()
+  const fakeUser = {
+    username: 'chucknorris',
+    id: 'abc-123',
+    token: fakeToken,
+  }
+  get.mockImplementation(url => {
+    if (url === '/auth/me') {
+      return Promise.resolve({data: {user: fakeUser}})
+    } else if (url === '/users') {
+      return Promise.resolve({data: {users: [fakeUser]}})
+    } else if (url === '/posts') {
+      return Promise.resolve({data: {posts: []}})
+    } else {
+      throw new Error(`Unexpected request to ${url}`)
+    }
+  })
+
+  const {container} = renderWithRouter(<App />)
+
+  // wait for /me request to settle
+  await flushPromises()
+  expect(container.firstChild).toMatchSnapshot()
+})
