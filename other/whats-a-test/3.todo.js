3998beef779986d76ead672e8be815ab6f92a27b
Ryan Huber <ryan.huber@mac.com>
2018-04-18 15:04:00 -0400

    add test() example to comments (#70)

--- other/whats-a-test/3.todo.js
+++ other/whats-a-test/3.todo.js
@@ -12,6 +12,13 @@ Then wrap each of your tests in a `test` function.
 This also means that we can run all the tests even
 if one of them fails!
 
+Example of test function
+test(title, () => {
+  // arrange
+  // act
+  // assert
+}
+
 Then run this code with `node 3.todo`
 
 > Make sure you're in the right directory!
